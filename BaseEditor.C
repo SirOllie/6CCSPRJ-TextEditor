bdc4f6756aef4a4a78c07bbf9c9945b4b508f962
Oliver Singer <owsinger@outlook.com>
2023-11-10 14:51:25 +0000

    Added placeholder println statement.

--- BaseEditor.C
+++ BaseEditor.C
@@ -1,3 +1,7 @@
 //Base Text Editor Implementation
 //Based off the kilo text editor C implementation by antirez and tutorial found at https://viewsourcecode.org/snaptoken/kilo/index.html
 //Oliver Singer
+int main() {
+  println("test");
+  return 0;
+}

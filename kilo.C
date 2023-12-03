fab1a6477545959fe60be10162a1e183a30b7c88
Olising <owsinger@outlook.com>
2023-12-03 21:58:57 +0000

    added to text editor

--- kilo.C
+++ kilo.C
@@ -2,6 +2,7 @@
 //Based off the kilo text editor C implementation by antirez and tutorial found at https://viewsourcecode.org/snaptoken/kilo/index.html
 //Oliver Singer
 
+/*** includes ***/
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -10,11 +11,11 @@
 #include <stdlib.h>
 #include <termios.h>
 
-//##################################################
+/*** data ***/
 
 struct termios orig_termios;
 
-//##################################################
+/*** terminal ***/
 
 void die(const char *s) {
   perror(s);
@@ -22,13 +23,12 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
-
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
-
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
@@ -36,15 +36,16 @@ void enableRawMode() {
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
-
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main() {
   enableRawMode();
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {

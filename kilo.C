68a3ac7e6ead1e012a8cf785867cee06ff9688c6
Olising <owsinger@outlook.com>
2023-12-04 19:29:28 +0000

    fixed error

--- kilo.C
+++ kilo.C
@@ -103,10 +103,10 @@ struct abuf {
 #define ABUF_INIT {NULL, 0}
 
 void abAppend(struct abuf *ab, const char *s, int len) {
-  char *new = realloc(ab->b, ab->len + len);
-  if (new == NULL) return;
-  memcpy(&new[ab->len], s, len);
-  ab->b = new;
+  char *new_ptr = realloc(ab->b, ab->len + len);
+  if (new_ptr == NULL) return;
+  memcpy(&new_ptr[ab->len], s, len);
+  ab->b = new_ptr;
   ab->len += len;
 }
 void abFree(struct abuf *ab) {

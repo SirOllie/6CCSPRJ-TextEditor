6fae252ae6559d152b05799f30441cc8c3d5f1a5
Olising <owsinger@outlook.com>
2023-12-04 21:09:03 +0000

    added

--- kilo.C
+++ kilo.C
@@ -170,7 +170,7 @@ void editorOpen(char *filename) {
                            line[linelen - 1] == '\r'))
       linelen--;
     E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
+    E.row.chars = (chars *)malloc(linelen + 1);
     memcpy(E.row.chars, line, linelen);
     E.row.chars[linelen] = '\0';
     E.numrows = 1;

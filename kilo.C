c65d4b2cddbd4ddc416d9baced4eb390200d9047
Olising <owsinger@outlook.com>
2023-12-04 15:08:58 +0000

    added to text editor

--- kilo.C
+++ kilo.C
@@ -57,14 +57,24 @@ void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
 }
 
 /*** output ***/
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows();
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
